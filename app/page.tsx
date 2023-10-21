8d02e6849d7b32a9972734b3477c94ba75a003d4
DAVEALLCAPS <daveallcaps@gmail.com>
2023-10-20 18:19:58 -0600

    Add Task List and Toast

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,44 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import Image from "next/image";
+import { useState, ChangeEvent } from "react";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function Home() {
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [task, setTask] = useState<string>("");
+  const { toast } = useToast(); // Use the useToast hook
+
+  const addTask = () => {
+    if (task.trim() !== "") {
+      setTasks([...tasks, task]);
+      toast({
+        title: "Task Added",
+        description: `Task "${task}" was successfully added to the list.`,
+      });
+      setTask("");
+    }
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTask(e.target.value);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <div className="space-y-2 justify-items-center">
       <h1 className="text-3xl p-4">To Do List</h1>
-      <div className="flex-col justify-center">
-        <Input />
-        <Button>Add Task</Button>
+      <div className="flex space-x-2">
+        <Input value={task} onChange={handleInputChange} />
+        <Button onClick={addTask}>Add Task</Button>
       </div>
-    </main>
+      <div className="space-y-2 mt-4">
+        {tasks.map((task, index) => (
+          <div key={index} className="p-2 border rounded">
+            {task}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
